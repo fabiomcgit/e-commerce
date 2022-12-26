9e350076d4a93972408c8f6afe32b76461216563
Fábio Menezes <fabiomc.git@gmail.com>
2022-12-26 15:37:25 -0300

    "Mudança de Idioma"

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -95,7 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'pt-BR'
+LANGUAGE_CODE = 'pt_BR'
 
 TIME_ZONE = 'UTC'
 
